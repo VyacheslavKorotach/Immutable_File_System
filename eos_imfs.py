63132a07f72b7ab384a9b9e73dff756bbc3b1dae
Vyacheslav Korotach <vkorotach@gmail.com>
2019-09-21 15:58:43 +0300

    git test

--- eos_imfs.py
+++ eos_imfs.py
@@ -1,1 +1,15 @@
-# first commit
+import os
+import json
+import time
+from eospy.cleos import Cleos
+import eospy.cleos
+import eospy.keys
+import pytz
+import requests
+
+class EosFile:
+    def __init__(self):
+        pass
+
+    def ping(self):
+        print("I'm here")
